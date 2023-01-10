a9bea9e72772babcc18deff3bd319764881a2102
MylesMburu <ngicumyles@gmail.com>
2023-01-10 15:45:05 +0300

    functional add button

--- index.js
+++ index.js
@@ -1,20 +1,78 @@
 function openTab(evt,tabId) {
-    // Declare all variables
+    //  Declare all variables
     var i, tabcontent, tablinks;
   
-    // Get all elements with class="tabcontent" and hide them
+    //  Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
+    //  Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
-    // Show the current tab, and add an "active" class to the button that opened the tab
+    //  Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabId).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+window.addEventListener('load', () => {
+  const form = document.getElementById('form');
+  const input = document.getElementsByClassName('task');
+  const list_el = document.getElementById('All');
+
+  form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const task = input[0].value;
+  const deadline = input[1].value;
+  
+   const task_el = document.createElement('div');
+   task_el.classList.add('task');
+
+   const task_content_el = document.createElement('div');
+   task_content_el.classList.add('content');
+  //  task_content_el.innerHTML=task + " " + deadline;
+
+   task_el.appendChild(task_content_el);
+
+   const task_input_el = document.createElement('input');
+   task_input_el.classList.add('text');
+   task_input_el.type = 'text';
+   task_input_el.value = task + " " +"by"+" "+ deadline;
+   task_input_el.setAttribute('readonly', 'readonly');
+
+   task_content_el.appendChild(task_input_el);
+
+   const task_actions_el = document.createElement('div');
+   task_actions_el.classList.add('actions');
+  
+   const task_edit_el = document.createElement('button');
+   task_edit_el.classList.add('edit');
+   task_edit_el.innerText = 'Edit';
+
+   const task_delete_el = document.createElement('button');
+   task_delete_el.classList.add('delete');
+   task_delete_el.innerText = 'Delete';
+
+   const task_done_el = document.createElement('button');
+   task_done_el.classList.add('done');
+   task_done_el.innerHTML = 'Done';
+
+   task_actions_el.appendChild(task_done_el);
+   task_actions_el.appendChild(task_edit_el);
+   task_actions_el.appendChild(task_delete_el);
+   
+   task_el.appendChild(task_actions_el);
+
+   list_el.appendChild(task_el);
+
+   input[0].value = '';
+   input[1].value = '';
+   window.alert("Task added to 'All")
+
+})
+})
